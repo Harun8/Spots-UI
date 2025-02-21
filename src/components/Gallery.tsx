c2da56e148d6d9c47af3c8402d01ccfe080807ea
Harun8 <harunabdi8@gmail.com>
2025-02-21 23:31:52 +0100

    yoyo

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,65 +1,7 @@
 import React, { useState } from 'react'
 
 import CardMaker from './CardMaker';
-const BOOKS = [
-    {
-      img: `/image/books/RectangleBig1.svg`,
-      category: "Natasha Wing",
-      title: "The Night Before Kindergarten",
-      desc: "A heartwarming and humorous picture book that eases the jitters of starting kindergarten.",
-      price: "$99",
-      offPrice: "$79",
-    },
-    {
-      img: `/image/books/RectangleBig6.svg`,
-      category: "James Patterson",
-      title: "Middle School: The Worst Years of My Life",
-      desc: "A funny and relatable novel about the challenges of navigating middle school.",
-      price: "$99",
-      offPrice: "$79",
-    },
-    {
-      img: `/image/books/RectangleBig2.svg`,
-      category: "Helen W. Colby",
-      title: "College Student: A Comprehensive Checklist",
-      desc: "A practical guidebook that helps college students prepare for the transition to university.",
-      price: "$99",
-      offPrice: "$79",
-    },
-    {
-      img: `/image/books/RectangleBig3.svg`,
-      category: "Walter Pauk",
-      title: "How to Study in College",
-      desc: "A valuable resource for high school seniors and college freshmen, offering effective study strategies.",
-      price: "$99",
-      offPrice: "$79",
-    },
-    {
-      img: `/image/books/RectangleBig4.svg`,
-      category: "William Strunk Jr.",
-      title: "The Elements of Style",
-      desc: "A classic reference book on grammar and writing skills, essential for high school and college students.",
-      price: "$99",
-      offPrice: "$79",
-    },
-    {
-      img: `/image/books/RectangleBig5.svg`,
-      category: "William Strunk Jr.",
-      title: "The Elements of Style",
-      desc: "A classic reference book on grammar and writing skills, essential for high school and college students.",
-      price: "$99",
-      offPrice: "$79",
-    },
-  ];
-  
-  const BOOKS_TABS = [
-    "history",
-    "law",
-    "math",
-    "economy",
-    "business",
-    "communication",
-  ];
+import { Button } from './ui/button';
 
 const Gallery = () => {
     const [activeTab, setActiveTab] = useState()
@@ -71,7 +13,14 @@ const Gallery = () => {
               Discover More Than Just Places
             </p>
 
-        <div className="mt-10 flex items-center justify-center">
+        <div className="mt-10 flex items-center justify-center gap-3">
+
+
+          <Button variant="default"> Click </Button>
+          <Button> Click </Button>
+          <Button> Click </Button>
+          <Button> Click </Button>
+
 
         </div>
       </div>
